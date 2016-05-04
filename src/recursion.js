37ef1155ccdf74524982d63cbcdf826144adf6a6
n-white <neil.r.whitehead@gmail.com>
2016-05-04 23:00:26 +0100

    "Sum Below solved"

--- src/recursion.js
+++ src/recursion.js
@@ -58,13 +58,13 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
-	if(n = 0) {
-		return -1;
-	} else if (n == 1) {
+	if(n == 0) {
 		return 0;
-	} else {
-		return n + sumBelow(n-1);
-	};
+	} else if (n > 0){
+		return n-1 + sumBelow(n-1);
+	} else if (n < 0) {
+		return n+1 + sumBelow(n+1)
+	}
 };
 
 // 6. Get the integers in range (x, y).
