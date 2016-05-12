0ec2fadb932c93e0daa64870f93126d9be839b8d
n-white <neil.r.whitehead@gmail.com>
2016-05-12 16:41:35 +0100

    "Return nth Fibonacci solved"

--- src/recursion.js
+++ src/recursion.js
@@ -319,6 +319,17 @@ var fibonacci = function(n) {
 // nthFibo(7); // 13
 // nthFibo(3); // 2
 var nthFibo = function(n) {
+
+	if(n < 0) {
+		return null;
+	} else if(n == 0) {
+		return 0;
+	} else if(n <= 2) {
+		return 1;
+	} else {
+		return nthFibo(n - 2) + nthFibo(n - 1)
+	}
+
 };
 
 // 26. Given an array of words, return a new array containing each word capitalized.
